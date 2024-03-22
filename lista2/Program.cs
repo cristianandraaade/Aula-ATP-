cb5d9730889b0710e16e4649a2a42319c6d1d502
Cristian Breno Andrade Marques <1470341@sga.pucminas.br>
2024-03-22 19:47:53 -0300

    fazendo lista 2

---
+++ lista2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+    class Program{
+        public static void Main(){
+            int selector;
+            Console.WriteLine("Digite a atividade de 1 a 10");
+            selector=int.Parse(Console.ReadLine());
+
+            switch (selector){
+                case 1:
+                int numeroA, numeroB;
+                Console.WriteLine("Digite dois números");
+                numeroA=int.Parse(Console.ReadLine());
+                numeroB=int.Parse(Console.ReadLine());
+                if (numeroA > numeroB){
+                    Console.WriteLine("O maior número é " + numeroA);                   
+                }
+                else{
+                    Console.WriteLine("O maior número é " + numeroB);
+                }
+                break;
+                
+                case 2:
+                int numeroA, numeroB;
+                Console.WriteLine("Digite dois número");
+                numeroA=int.Parse(Console.ReadLine());
+                numeroB=int.Parse(Console.ReadLine());
+            }
+        
+            
+        }
+    }
