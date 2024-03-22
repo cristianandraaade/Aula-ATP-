40fc2a2b38ae38f8c5cc01a36cea152d72ebfd52
Cristian Breno Andrade Marques <1470341@sga.pucminas.br>
2024-03-22 20:22:30 -0300

    fazendo lista2

---
+++ lista2/Exercicio 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+
+        //declaração de variaveis
+        int numeroA, numeroB;
+        //Entrada de dados
+        Console.WriteLine("Digite dois números");
+        numeroA = int.Parse(Console.ReadLine());
+        numeroB = int.Parse(Console.ReadLine());
+        //Processamento e saida de dados
+        if (numeroA > numeroB)
+        {
+            Console.WriteLine("O maior número é " + numeroA);
+        }
+        else
+        {
+            Console.WriteLine("O maior número é " + numeroB);
+        }
+    }
+}
---
+++ lista2/Exercicio 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        //declaração de variaveis
+        int numeroA,numeroB,resultadoAdicao;
+        //Entrada de dados
+        Console.WriteLine("Digite dois número");
+        numeroA = int.Parse(Console.ReadLine());
+        numeroB = int.Parse(Console.ReadLine());
+        //Processamento e saida de dados
+        resultadoAdicao = numeroA + numeroB;
+        if (resultadoAdicao >= 10)
+        {
+            int condicao1 = (resultadoAdicao + 5);
+            Console.WriteLine("O valor é " + condicao1);
+        }
+        else
+        {
+            int condicao2 = (resultadoAdicao + 7);
+            Console.WriteLine("O valor é " + condicao2);
+        }
+    }
+}
