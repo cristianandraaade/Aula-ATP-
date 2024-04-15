14ef7fd73a4641d0b2ef9f1f05b8940cf92acfe6
Cristian Breno Andrade Marques <cristianbrenoandrade@gmail.com>
2024-04-15 18:09:44 -0300

    exericio 1

--- lista3/Exercicio 1/Program.cs
+++ lista3/Exercicio 1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+    class Program{
+        public static void Main(){
+            // declarão de variaveis
+            int numeroDigitado=0,numerosPositivos=-1,NumerosNegativos=0,zeros=0;
+            int stop=007;
+            while (numeroDigitado != stop){
+                Console.WriteLine("Digite números inteiro(Digite P para encerrar)");
+                numeroDigitado=int.Parse(Console.ReadLine());
+                if (numeroDigitado == 0){
+                    zeros ++;
+                }
+                else if (numeroDigitado >0){
+                    numerosPositivos++;
+                }
+                else {
+                    NumerosNegativos++;
+                }
+            }
+            Console.WriteLine("Números positivos: " + numerosPositivos);
+            Console.WriteLine("Números negativos: " + NumerosNegativos);
+            Console.WriteLine("Zeros: " + zeros);
+        }
+    }
