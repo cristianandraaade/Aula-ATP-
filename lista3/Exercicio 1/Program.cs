26004e8eeff861b251bcb90e332dfbe8ea0193e4
Cristian Breno Andrade Marques <1470341@sga.pucminas.br>
2024-04-22 21:56:43 -0300

    lista 3 fazendo

--- lista3/Exercicio 1/Program.cs
+++ lista3/Exercicio 1/Program.cs
@@ -1,24 +1,34 @@
 ﻿using System;
-    class Program{
-        public static void Main(){
-            // declarão de variaveis
-            int numeroDigitado=0,numerosPositivos=-1,NumerosNegativos=0,zeros=0;
-            int stop=007;
-            while (numeroDigitado != stop){
-                Console.WriteLine("Digite números inteiro(Digite 007 para encerrar)");
-                numeroDigitado=int.Parse(Console.ReadLine());
-                if (numeroDigitado == 0){
-                    zeros ++;
+class Program
+{
+    public static void Main()
+    {
+        // declarão de variaveis
+        int numeroDigitado = 0, numerosPositivos = 0, NumerosNegativos = 0, zeros = 0, total = 0;
+        int stop = 1234;
+        do
+        {
+            Console.WriteLine("Digite números inteiro");
+            numeroDigitado = int.Parse(Console.ReadLine());
+            if (numeroDigitado != stop)
+            {
+                if (numeroDigitado == 0)
+                {
+                    zeros++;
                 }
-                else if (numeroDigitado >0){
+                else if (numeroDigitado > 0)
+                {
                     numerosPositivos++;
                 }
-                else {
+                else
+                {
                     NumerosNegativos++;
                 }
             }
-            Console.WriteLine("Números positivos: " + numerosPositivos);
-            Console.WriteLine("Números negativos: " + NumerosNegativos);
-            Console.WriteLine("Zeros: " + zeros);
-        }
+
+        } while (numeroDigitado != stop);
+        Console.WriteLine("Números positivos: " + numerosPositivos);
+        Console.WriteLine("Números negativos: " + NumerosNegativos);
+        Console.WriteLine("Zeros: " + zeros);
     }
+}
--- lista3/Exercicio 2/Program.cs
+++ lista3/Exercicio 2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        // declarão de variaveis
+        int numeroDigitado = 0, numerosPositivos = 0, NumerosNegativos = 0, zeros = 0, total = 0;
+        int stop = 1234;
+        do
+        {
+            Console.WriteLine("Digite números inteiro");
+            numeroDigitado = int.Parse(Console.ReadLine());
+            if (numeroDigitado != stop)
+            {
+                if (numeroDigitado == 0)
+                {
+                    zeros++;
+                }
+                else if (numeroDigitado > 0)
+                {
+                    numerosPositivos++;
+                }
+                else
+                {
+                    NumerosNegativos++;
+                }
+                total++;
+            }
+
+        } while (numeroDigitado != stop);
+        Console.WriteLine("Números positivos: " + numerosPositivos);
+        Console.WriteLine("Números negativos: " + NumerosNegativos);
+        Console.WriteLine("Zeros: " + zeros);
+        Console.WriteLine("total " + total);
+    }
+}
