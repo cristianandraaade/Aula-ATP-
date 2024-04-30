c3fca88f173ef030bf6f8cdcbe9c0be3139a703b
Cristian Breno Andrade Marques <1470341@sga.pucminas.br>
2024-04-29 21:49:05 -0300

    exericio 4

---
+++ lista3/Exercicio 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int numeroDigitado = 0, fatorialNumero = 1;
+        Console.WriteLine("Digite um número para o calculo de fatorial");
+        numeroDigitado = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= numeroDigitado; i++)
+        {
+            if (numeroDigitado != 0)
+            {
+                fatorialNumero *= i;
+            }
+            else
+                Console.WriteLine("O fatorial de 0 é 1");
+        }
+        Console.WriteLine("A fatorial de {0} é: {1}", numeroDigitado, fatorialNumero);
+    }
+}
