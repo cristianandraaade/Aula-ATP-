92f83f22cd859f5d1129eda51e3afc14c3ac1406
Cristian Breno Andrade Marques <1470341@sga.pucminas.br>
2024-04-29 21:50:55 -0300

    exericio 4

--- lista3/Exercicio 2/Program.cs
+++ lista3/Exercicio 2/Program.cs
@@ -4,8 +4,11 @@ class Program
     public static void Main()
     {
         // declarão de variaveis
-        int numeroDigitado = 0, numerosPositivos = 0, NumerosNegativos = 0, zeros = 0, total = 0;
+        int numeroDigitado = 0, numerosPositivos = 0, numerosNegativos = 0, zeros = 0, total = 0;
+        float percentPositivos=0,percentNegativos=0,percentZeros=0;
         int stop = 1234;
+        //entrada de dados e processamento
+        // entra numeros e valida seu tipo
         do
         {
             Console.WriteLine("Digite números inteiro");
@@ -22,15 +25,24 @@ class Program
                 }
                 else
                 {
-                    NumerosNegativos++;
+                    numerosNegativos++;
                 }
                 total++;
             }
 
         } while (numeroDigitado != stop);
+        //calcula a porcentagem dos tipos de numeros baseados no total
+        
+        percentZeros=(float)(total/zeros)*10;
+        percentPositivos=(float)(total/numerosPositivos)*10;
+        percentNegativos=(float)(total/numerosNegativos)*10;
+        //saida de dados
         Console.WriteLine("Números positivos: " + numerosPositivos);
-        Console.WriteLine("Números negativos: " + NumerosNegativos);
+        Console.WriteLine("Números negativos: " + numerosNegativos);
         Console.WriteLine("Zeros: " + zeros);
         Console.WriteLine("total " + total);
+        Console.WriteLine("Porcentagem de zeros " + percentZeros + "%");
+        Console.WriteLine("Porcentagem de Positivos " + percentPositivos + "%");
+        Console.WriteLine("Porcentagem de Negativos " + percentNegativos + "%");
     }
 }
---
+++ lista3/Exercicio 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+    class Program{
+        public static void Main(){
+            int numeroDigitado=0, numerosDivisiveis3e9=0,numerosDivisiveis2e5=0;
+            Console.WriteLine("Insira 10 números");
+            for(int i =1; i <=10; i++){
+                numeroDigitado=int.Parse(Console.ReadLine());
+                Console.WriteLine(i);
+                if (numeroDigitado % 2 == 0)&&(numeroDigitado % 5 == 0){
+                    numerosDivisiveis2e5++;
+                }
+                else if(numeroDigitado % 3 == 0)&&(numeroDigitado % 9 == 0){
+                    numerosDivisiveis3e9++;
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+    }
