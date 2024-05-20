11fab3430abaf9cb8b3a09f78dc7facbe065af4e
Cristian Breno Andrade Marques <cristianbrenoandrade@gmail.com>
2024-05-20 17:48:07 -0300

    exercicio 8

--- lista4/Exercicio7.cs
+++ lista4/Exercicio7.cs
@@ -1,6 +1,53 @@
 using System;
-public class Exercicio7{
-    public void Run(){
-        Console.WriteLine("Eu sou exercicio 7");
+public class Exercicio7
+{
+    public void Run()
+    {
+        int mediaAlunos, quantidadeAlunos;
+
+        Console.WriteLine("Digite o número de alunos");
+        quantidadeAlunos = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= quantidadeAlunos; i++)
+        {
+            Console.WriteLine("Digite a média do aluno");
+            mediaAlunos = int.Parse(Console.ReadLine());
+            AnalisaMedia(mediaAlunos);
+        }
+    }
+    static void AnalisaMedia(int mediaAlunos)
+    {
+        char conceito;
+
+        if (mediaAlunos <= 39)
+        {
+            conceito = 'F';
+            Console.WriteLine("Sua média foi {0} e seu conceito foi {1}", mediaAlunos, conceito);
+
+        }
+        else if ((mediaAlunos >=40)&&(mediaAlunos <=59))
+        {
+            conceito = 'E';
+            Console.WriteLine("Sua média foi {0} e seu conceito foi {1}", mediaAlunos, conceito);
+        }
+        else if ((mediaAlunos >=60)&&(mediaAlunos <=69))
+        {
+            conceito = 'D';
+            Console.WriteLine("Sua média foi {0} e seu conceito foi {1}", mediaAlunos, conceito);
+        }
+        else if ((mediaAlunos >=70)&&(mediaAlunos <=79))
+        {
+            conceito = 'C';
+            Console.WriteLine("Sua média foi {0} e seu conceito foi {1}", mediaAlunos, conceito);
+        }
+        else if ((mediaAlunos >=80)&&(mediaAlunos <=89))
+        {
+            conceito = 'B';
+            Console.WriteLine("Sua média foi {0} e seu conceito foi {1}", mediaAlunos, conceito);
+        }
+        else
+        {
+            conceito = 'A';
+            Console.WriteLine("Sua média foi {0} e seu conceito foi {1}", mediaAlunos, conceito);
+        }
     }
 }
